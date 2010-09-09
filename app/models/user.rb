0b3d6d4cdad96f1054257402af8064e966e1a35f
mrajendran <rajmanjula2006@gmail.com>
2010-09-09 19:17:43 +0530

    added the annotate models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+# Schema version: 20100830153925
+#
+# Table name: users
+#
+#  id                :integer(4)      not null, primary key
+#  created_at        :datetime        
+#  updated_at        :datetime        
+#  login             :string(255)     not null
+#  crypted_password  :string(255)     not null
+#  password_salt     :string(255)     not null
+#  persistence_token :string(255)     not null
+#  login_count       :integer(4)      default(0), not null
+#  last_request_at   :datetime        
+#  last_login_at     :datetime        
+#  current_login_at  :datetime        
+#  last_login_ip     :string(255)     
+#  current_login_ip  :string(255)     
+#  active            :boolean(1)      not null
+#  perishable_token  :string(255)     not null
+#  email             :string(255)     not null
+#
+
 class User < ActiveRecord::Base
   acts_as_authentic
   has_friendly_id :login, :use_slug => true
